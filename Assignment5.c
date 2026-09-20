a08b342d47e7a1536e59627886392445db8959a1
armine-panosyan <53708246+armine-panosyan@users.noreply.github.com>
2026-09-20 23:18:12 +0400

    Update Assignment5.c

--- Assignment5.c
+++ Assignment5.c
@@ -19,3 +19,27 @@ int main() {
     }
     return 0;
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        printf("Fork failed\n");
+        return 1;
+    }
+    if (pid == 0) {
+        printf("Child PID: %d\n", getpid());
+        exit(0);
+    } else {
+        printf("Parent PID: %d\n", getpid());
+        wait(NULL);
+        printf("Child finished\n");
+        sleep(30);
+    }
+
+    return 0;
+}
